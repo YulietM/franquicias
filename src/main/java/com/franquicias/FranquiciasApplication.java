64a90e4ce0b71dc1864f6b83c7a1cff571232fd0
Yuliet Murcia <am514868@gmail.com>
2024-08-11 11:03:26 -0500

    commit initial

---
+++ src/main/java/com/franquicias/FranquiciasApplication.java
@@ -0,0 +1,13 @@
+package com.franquicias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranquiciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranquiciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franquicias/FranquiciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franquicias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
